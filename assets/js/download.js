12a5f82d5143d78ef764cb7ae7e01db26f7d4eab
CircusDad <CypherDad@protonmail.com>
2024-09-27 20:48:13 -0400

    Update consistency between js and html

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,16 +1,20 @@
 function openDownloadTab(evt, cityName) {
+    // Declare all variables
     var i, tabcontent, tablinks;
 
+    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
+    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
+    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
@@ -24,7 +28,8 @@ function getOS() {
     } else if (!os && /Linux/.test(platform)) {
         os = 'linux';
     } else {
-        os = "windows"; // default to Windows
+        // show Windows by default
+        os = "windows";
     }
     return os;
 }
